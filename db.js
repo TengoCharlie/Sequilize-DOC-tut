c4004d99709684386853d959b1c4e33cfbfbfc6d
Harsh Tenguriya <harsh@akeo.no>
2021-06-27 02:35:38 +0530

    Connection Method

---
+++ db.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+// Sequelize connection and authentication link setup
+const sequelize = new Sequelize('school', 'postgres', '123456789', {
+    dialect: 'postgres',
+    host: 'localhost',
+    logging: (...msg) => console.log(msg)
+}).authenticate().then(() => {
+    console.log('Connection has been established');
+}).catch(err => console.error(`Error connecting  :  ${error}  `))
+
+module.exports = sequelize;
+
