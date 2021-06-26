6abd996a2ca8da8478e6f9dd65fedf44a7c8be98
Harsh Tenguriya <harsh@akeo.no>
2021-06-27 04:35:00 +0530

    Column Options'

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
-const { Sequelize, DataTypes, Model } = require("sequelize");
+const { Model, DataTypes, Deferrable, Sequelize } = require("sequelize");
 
-// Sequelize connection and authentication link setup
 const sequelize = new Sequelize("school", "postgres", "123456789", {
   dialect: "postgres",
   host: "localhost",
@@ -18,55 +17,96 @@ sequelize
   })
   .catch((err) => console.error(`Error connecting  :  ${error}  `));
 
-// Model Defination =============================
-// Using Define method
-
-const User = sequelize.define(
-  "User",
+class Bar extends Model {}
+Bar.init(
   {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: Sequelize.UUIDV4, // Or Sequelize.UUIDV1 AUtomatically insert a Unique ID in it
-      primaryKey: true,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-    },
+    // instantiating will automatically set the flag to true if not set
+    flag: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
+
+    // default values for dates => current time
+    myDate: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+
+    // setting allowNull to false will add NOT NULL to the column, which means an error will be
+    // thrown from the DB when the query is executed if the column is null. If you want to check that a value
+    // is not null before querying the DB, look at the validations section below.
+    title: { type: DataTypes.STRING, allowNull: false },
   },
   {
-    freezeTableName: true, //To prevent sql to rename the table according to it (This is for perticular table)
-    // timestamps: false, //to desable created at and updated at
+    sequelize,
+    modelName: "Bar",
   }
 );
 
-const Person = sequelize.define("Person", {
-  name: {
-    type: DataTypes.STRING,
-    defaultValue: "John Doe",
+class Foo extends Model {}
+Foo.init(
+  {
+    // instantiating will automatically set the flag to true if not set
+    flag: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
+
+    // default values for dates => current time
+    myDate: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+
+    // setting allowNull to false will add NOT NULL to the column, which means an error will be
+    // thrown from the DB when the query is executed if the column is null. If you want to check that a value
+    // is not null before querying the DB, look at the validations section below.
+    title: { type: DataTypes.STRING, allowNull: false },
+
+    // Creating two objects with the same value will throw an error. The unique property can be either a
+    // boolean, or a string. If you provide the same string for multiple columns, they will form a
+    // composite unique key.
+    uniqueOne: { type: DataTypes.STRING, unique: "compositeIndex" },
+    uniqueTwo: { type: DataTypes.INTEGER, unique: "compositeIndex" },
+
+    // The unique property is simply a shorthand to create a unique constraint.
+    someUnique: { type: DataTypes.STRING, unique: true },
+
+    // Go on reading for further information about primary keys
+    identifier: { type: DataTypes.STRING, primaryKey: true },
+
+    // autoIncrement can be used to create auto_incrementing integer columns
+    incrementMe: { type: DataTypes.INTEGER, autoIncrement: true },
+
+    // You can specify a custom column name via the 'field' attribute:
+    fieldWithUnderscores: {
+      type: DataTypes.STRING,
+      field: "field_with_underscores",
+    },
+
+    // It is possible to create foreign keys:
+    bar_id: {
+      type: DataTypes.INTEGER,
+
+      references: {
+        // This is a reference to another model
+        model: Bar,
+
+        // This is the column name of the referenced model
+        key: "id",
+
+        // With PostgreSQL, it is optionally possible to declare when to check the foreign key constraint, passing the Deferrable type.
+        deferrable: Deferrable.INITIALLY_IMMEDIATE,
+        // Options:
+        // - `Deferrable.INITIALLY_IMMEDIATE` - Immediately check the foreign key constraints
+        // - `Deferrable.INITIALLY_DEFERRED` - Defer all foreign key constraint check to the end of a transaction
+        // - `Deferrable.NOT` - Don't defer the checks at all (default) - This won't allow you to dynamically change the rule in a transaction
+      },
+    },
+
+    // Comments can only be added to columns in MySQL, MariaDB, PostgreSQL and MSSQL
+    commentMe: {
+      type: DataTypes.INTEGER,
+      comment: "This is a column name that has a comment",
+    },
   },
-});
+  {
+    sequelize,
+    modelName: "foo",
 
-const Foo = sequelize.define("Foo", {
-  bar: {
-    type: DataTypes.DATE,
-    defaultValue: Sequelize.NOW,
-    // This way, the current date/time will be used to populate this column (at the moment of insertion)
-  },
-});
+    // Using `unique: true` in an attribute above is exactly the same as creating the index in the model's options:
+    indexes: [{ unique: true, fields: ["someUnique"] }],
+  }
+);
 
-// Model Syncronization
-// User.sync({ force: true }); //this is mendatory to use this to create table in DB or to recreate table
-
-// Sync all models
-sequelize.sync({ force: true }); //This method is use to sync all models in the table
-
-// User.drop(); //to drop table
-// sequelize.drop(); //to drop all tables
-
-console.log(User === sequelize.models.User);
+sequelize.sync({ force: true });
 
 module.exports = sequelize;
