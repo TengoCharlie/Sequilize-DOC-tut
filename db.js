b2a46d84e7184b13eaa8b7b8a715e3b44209ae5c
Harsh Tenguriya <harsh@akeo.no>
2021-06-27 03:02:12 +0530

    Connection and Authentication

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require("sequelize");
 
 // Sequelize connection and authentication link setup
-const sequelize = new Sequelize('school', 'postgres', '123456789', {
-    dialect: 'postgres',
-    host: 'localhost',
-    logging: (...msg) => console.log(msg)
-}).authenticate().then(() => {
-    console.log('Connection has been established');
-}).catch(err => console.error(`Error connecting  :  ${error}  `))
+const sequelize = new Sequelize("school", "postgres", "123456789", {
+  dialect: "postgres",
+  host: "localhost",
+  logging: (...msg) => console.log(msg),
+});
+
+// Authentication connected or not
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established");
+  })
+  .catch((err) => console.error(`Error connecting  :  ${error}  `));
 
 module.exports = sequelize;
-
